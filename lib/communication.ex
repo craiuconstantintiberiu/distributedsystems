c21354be19aa14ee508259c0a1db47a638162f6e
Constantin-Tiberiu Craiu <57532657+craiuconstantintiberiu@users.noreply.github.com>
2023-06-15 00:17:57 +0300

    Small changes

--- lib/communication.ex
+++ lib/communication.ex
@@ -4,135 +4,65 @@ defmodule CommunicationServer do
   require Logger
 
   def start_link(opts \\ []) do
-    GenServer.start_link(__MODULE__, opts)
+    #name = String.to_atom("server_#{opts[:port]}")
+    GenServer.start_link(__MODULE__, {:init_state, opts}, name: {:local, opts[:port]})
+    #GenServer.start_link(__MODULE__, {:init_state, opts}, name: {:local, opts[:port]})
   end
 
-  def send(port, index) do
-    GenServer.cast(__MODULE__, {:send, port, index})
+  def sendCom(port, message) do
+    GenServer.cast({:local, port}, {:send, message})
   end
 
   def accept(port) do
-    GenServer.cast(__MODULE__, {:accept, port})
+    GenServer.cast({:local, port}, :accept)
   end
 
-  defp serve(socket) do
-    msg = read_line(socket)
-
-    IO.inspect(msg)
-
-    x = Main.Message.decode(msg)
-    IO.inspect(x)
-
-    serve(socket)
+  def init({:init_state, opts}) do
+    {:ok, socket} = :gen_tcp.listen(opts[:port], [:binary, active: false, reuseaddr: true])
+    sendCom(self(), {:send, create_message(opts[:port], opts[:index])})
+    {:ok, {socket, opts}}
   end
 
-  defp read_line(socket) do
-    {:ok, <<_rest::binary-size(4), encoded::binary-size(15), _rest2::binary-size(4)>>} = :gen_tcp.recv(socket, 0)
-    # {:ok,<<data::binary-size(4)>>} = :gen_tcp.recv(socket, 4)
-    # size = :binary.decode_unsigned(data)
-    # {:ok, encoded} = :gen_tcp.recv(socket,size)
-    # IEx.pry()
-    message = Main.Message.decode(encoded)
-    # message = Protobuf.decode(encoded, Main.Message)
-    IO.inspect(message)
-    message
+  defp create_message(port, index) do
+    # Your message creation logic goes here
   end
 
-  defp write_line(line, socket) do
-    :gen_tcp.send(socket, line)
-  end
-
-  def init(opts) do
-    Logger.info("Starting CommunicationServer on port #{opts[:port]} with index #{opts[:index]}")
-    host = "127.0.0.1"
-    struct = %Main.Message{
-      :type => 0,
-      :networkMessage => %Main.NetworkMessage{
-        :senderHost => "127.0.0.1",
-        :senderListeningPort => opts[:port],
-        :message => %Main.Message{
-          :type => 1,
-          :procRegistration => %Main.ProcRegistration{
-            :owner => "tibi",
-            :index => opts[:index]
-          }
-        }
-      }
-    }
-    encoded = Main.Value.encode(struct)
+  def handle_cast({:send, message}, {socket, _opts}) do
+    encoded = Main.Message.encode(message)
     length = byte_size(encoded)
-    bits = Integer.to_string(length, 2)
 
     msg = <<0, 0, 0, length>> <> encoded
 
-    {:ok, socket} = :gen_tcp.connect('localhost', 5000, [:binary])
     :ok = :gen_tcp.send(socket, msg)
     :ok = :gen_tcp.close(socket)
 
-    Logger.info("Sent registration message to master")
-
-
-    # {:ok, _socket} = :gen_tcp.listen(opts[:port], [:binary, packet: :line, active: false, reuseaddr: true])
-    # Logger.info("Accepting connections on port #{opts[:port]}")
-    {:ok, opts}
+    {:noreply, {socket, _opts}}
   end
 
-  def handle_cast({:send, port, index}, _state) do
-    host = "127.0.0.1"
-    struct = %Main.Message{
-      :type => 0,
-      :networkMessage => %Main.NetworkMessage{
-        :senderHost => "127.0.0.1",
-        :senderListeningPort => port,
-        :message => %Main.Message{
-          :type => 1,
-          :procRegistration => %Main.ProcRegistration{
-            :owner => "tibi",
-            :index => index
-          }
-        }
-      }
-    }
-    encoded = Main.Value.encode(struct)
-    length = byte_size(encoded)
-    bits = Integer.to_string(length, 2)
+  def handle_cast(:accept, {socket, _opts}) do
+    case :gen_tcp.accept(socket) do
+      {:ok, client_socket} ->
+        Logger.info("Accept worked!")
+        # Process the client socket as necessary
+      {:error, _} ->
+        # Handle error
+    end
 
-    msg = <<0, 0, 0, length>> <> encoded
-
-    {:ok, socket} = :gen_tcp.connect('localhost', 5000, [:binary])
-    :ok = :gen_tcp.send(socket, msg)
-    :ok = :gen_tcp.close(socket)
-
-    {:noreply, nil}
+    {:noreply, {socket, _opts}}
   end
 
-  def handle_cast({:accept, port}, _state) do
-    {:ok, socket} = :gen_tcp.accept(port, [:binary, packet: :line, active: false, reuseaddr: true])
-    Logger.info("Accept worked!")
-    IO.inspect(socket)
-    serve(socket)
-    {:noreply, nil}
-  end
-
-  #create a handler for the :tcp message
-  def handle_info({:tcp, socket, data}, state) do
-    Logger.info("Data: #{inspect data}")
-    # Get the size of the message
-
-    <<_rest::binary-size(4), encoded::binary>> = data
-    Logger.info("Encoded: #{inspect encoded}")
-    message = Protobuf.decode(encoded, Main.Message)
-    # message = Main.Message.decode(encoded)
-    Logger.info("Message: #{inspect message}")
+  def handle_info(msg, state) do
+    Logger.warn("Received unexpected message: #{inspect(msg)}")
     {:noreply, state}
   end
 
   def handle_info({:tcp_error, socket, reason}, state) do
     Logger.info("Received error on socket #{inspect socket}")
-    end
+    {:noreply, state}
+  end
 
-    def handle_info({:tcp_closed, socket}, state) do
-      Logger.info("Received closed on socket #{inspect socket}")
-    end
-
+  def handle_info({:tcp_closed, socket}, state) do
+    Logger.info("Received closed on socket #{inspect socket}")
+    {:noreply, state}
+  end
 end
--- lib/start.ex
+++ lib/start.ex
@@ -1,23 +1,20 @@
 defmodule Start do
-  require CommunicationServer
+  @moduledoc """
+  Starts Communication Servers on specific ports.
+  """
+
   def start do
-    # sapwn three communication servers on different ports
-    #get pid of each server
-    {:ok, pid1} = CommunicationServer.start_link([port: 5004, index: 1])
-    {:ok, pid2} = CommunicationServer.start_link([port: 5005, index: 2])
-    {:ok, pid3} = CommunicationServer.start_link([port: 5006, index: 3])
+    start_servers([
+      [port: 5004, index: 1],
+      [port: 5005, index: 2],
+      [port: 5006, index: 3]
+    ])
+  end
 
-    #create a tcp listener on each port that is active and has controlling process
-    {:ok, socket1} = :gen_tcp.listen(5004, [:binary, active: true, reuseaddr: true])
-    {:ok, socket2} = :gen_tcp.listen(5005, [:binary, active: true, reuseaddr: true])
-    {:ok, socket3} = :gen_tcp.listen(5006, [:binary, active: true, reuseaddr: true])
+  defp start_servers([]), do: :ok
 
-    {:ok, socket1} = :gen_tcp.accept(socket1)
-    {:ok, socket2} = :gen_tcp.accept(socket2)
-    {:ok, socket3} = :gen_tcp.accept(socket3)
-
-    :gen_tcp.controlling_process(socket1, pid1)
-    :gen_tcp.controlling_process(socket2, pid2)
-    :gen_tcp.controlling_process(socket3, pid3)
+  defp start_servers([opts | rest]) do
+    {:ok, _pid} = CommunicationServer.start_link(opts)
+    start_servers(rest)
   end
 end
